9822d487bb9916196f21c67b92c8ffb8adf5260f
khar20 <136043524+khar20@users.noreply.github.com>
2026-02-17 11:57:05 -0500

    fix api url prefix

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("core.urls")),
+    path("", include("core.urls")),
 ]
